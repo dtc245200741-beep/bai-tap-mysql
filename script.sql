18c1a9f05942f24d883a110258797833b7eb2537
dtc245200741-beep <dtc245200741@ictu.edu.vn>
2026-09-20 15:15:57 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,8 +1,16 @@
-CREATE DATABASE demo;
-USE demo;
+-- Sử dụng hoặc tạo mới CSDL student-management nếu chưa có
+CREATE DATABASE IF NOT EXISTS `student-management`;
+USE `student-management`;
 
-CREATE TABLE Student (
-    id INT,
+-- Tạo bảng Class
+CREATE TABLE Class (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200)
+);
+
+-- Tạo bảng Teacher
+CREATE TABLE Teacher (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(200),
     age INT,
     country VARCHAR(50)
