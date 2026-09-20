37245973ee0f39dd7990753f9c71f84329984888
dtc245200741-beep <dtc245200741@ictu.edu.vn>
2026-09-20 15:12:01 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE demo;
 USE demo;
 
-CREATE TABLE Student(
- id INT,
- name VARCHAR(200),
- age INT,
- country VARCHAR(50)
+CREATE TABLE Student (
+    id INT,
+    name VARCHAR(200),
+    age INT,
+    country VARCHAR(50)
 );
